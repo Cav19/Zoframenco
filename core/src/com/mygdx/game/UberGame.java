a97c1131b9cbd20717e05aff45f1a9892f096697
Valenti <=>
2016-02-23 19:57:47 -0600

    Added function car image movement.

--- core/src/com/mygdx/game/UberGame.java
+++ core/src/com/mygdx/game/UberGame.java
@@ -2,20 +2,34 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class UberGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	private Texture taxiImg;
-	private Texture map;
+	private Texture mapImg;
+	private OrthographicCamera camera;
+	private Rectangle car;
+	private Rectangle map;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		map = new Texture("map.jpg");
+		car = new Rectangle();
+		car.x = 800 / 2;
+		car.y = 480 / 2;
+		car.width = 64;
+		car.height = 64;
 		taxiImg = new Texture("taxi.png");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		mapImg = new Texture("map.jpg");
+
 	}
 
 	@Override
@@ -23,8 +37,14 @@ public class UberGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(map, 0, 0);
-		batch.draw(taxiImg, 0, 0);
+		batch.draw(mapImg, 0, 0);
+		batch.draw(taxiImg, car.x, car.y);
 		batch.end();
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) car.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) car.x += 200 * Gdx.graphics.getDeltaTime();
+
+		if(car.x < 0) car.x = 0;
+		if(car.x > 800 - 64) car.x = 800 - 64;
 	}
 }
