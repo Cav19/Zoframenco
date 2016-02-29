f1594f3fbed364669be4e5304bc11dab0630a245
FRANCESCO NUTRICATO <FRANCESCO NUTRICATO>
2016-02-28 18:52:56 -0600

    Added code to show tiled map

--- core/src/com/mygdx/game/UberGame.java
+++ core/src/com/mygdx/game/UberGame.java
@@ -1,50 +1,87 @@
+/*Credits: Tiledmap implementation instructions and parts of code from
+ http://www.gamefromscratch.com/post/2014/04/16/LibGDX-Tutorial-11-Tiled-Maps-Part-1-Simple-Orthogonal-Maps.aspx
+ on 2/27/2016
+
+
+*/
+
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.intellij.ide.actions.ToggleFullScreenAction;
+import com.sun.jna.platform.unix.X11;
 
 public class UberGame extends ApplicationAdapter {
+
+
+
 	SpriteBatch batch;
 	private Texture taxiImg;
-	private Texture mapImg;
 	private OrthographicCamera camera;
 	private Rectangle car;
-	private Rectangle map;
-	
+	TiledMap tiledMap;
+	TiledMapRenderer tiledMapRenderer;
+
 	@Override
 	public void create () {
+
+
 		batch = new SpriteBatch();
 		car = new Rectangle();
-		car.x = 800 / 2;
-		car.y = 480 / 2;
-		car.width = 64;
+		float width = Gdx.graphics.getWidth();
+		float height = Gdx.graphics.getHeight();
+		car.x = width/2;
+		car.y = height/2;
+		car.width = 28;
 		car.height = 64;
-		taxiImg = new Texture("taxi.png");
+		taxiImg = new Texture("tiny_car.png");
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-		mapImg = new Texture("map.jpg");
+		camera.setToOrtho(false, width, height);
+
+		tiledMap = new TmxMapLoader().load("tmpRoad.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		camera.update();
+
+
 
 	}
 
 	@Override
 	public void render () {
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
+
+
 		batch.begin();
-		batch.draw(mapImg, 0, 0);
 		batch.draw(taxiImg, car.x, car.y);
 		batch.end();
 
 		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) car.x -= 200 * Gdx.graphics.getDeltaTime();
 		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) car.x += 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) car.y += 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) car.y -= 200 * Gdx.graphics.getDeltaTime();
+
 
 		if(car.x < 0) car.x = 0;
-		if(car.x > 800 - 64) car.x = 800 - 64;
+		if(car.x > Gdx.graphics.getWidth() - car.width) car.x = Gdx.graphics.getWidth() - car.width;
+
+		if(car.y < 0) car.y = 0;
+		if(car.y > Gdx.graphics.getHeight() - car.height) car.y = Gdx.graphics.getHeight() - car.height;
 	}
 }
