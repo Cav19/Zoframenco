e1eaa183644420d2704b7000ddca494c8aae2b68
fnutrica <fnutrica@macalester.edu>
2016-04-08 11:35:49 -0500

    fix bug?

---
+++ core/assets/collision_Tile.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="collision_Tile" tilewidth="10" tileheight="10" tilecount="9" columns="3">
+ <image source="collision_Tile.jpg" trans="ffc4c4" width="32" height="32"/>
+ <terraintypes>
+  <terrain name="road" tile="-1"/>
+ </terraintypes>
+ <tile id="0">
+  <properties>
+   <property name="road" value="road"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="road" value="road"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="road" value="road"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="road" value="road"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="road" value="road"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="road" value="road"/>
+  </properties>
+ </tile>
+ <tile id="6" terrain=",0,,">
+  <properties>
+   <property name="road" value="road"/>
+  </properties>
+ </tile>
+ <tile id="7" terrain="0,0,0,0">
+  <properties>
+   <property name="road" value="road"/>
+  </properties>
+ </tile>
+ <tile id="8" terrain="0,0,0,0">
+  <properties>
+   <property name="road" value="road"/>
+  </properties>
+ </tile>
+</tileset>
