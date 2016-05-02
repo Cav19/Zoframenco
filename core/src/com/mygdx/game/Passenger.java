5aa2891426609d8c57c2745fbdd5e659390849ba
Valenti <cvalenti@macalester.edu>
2016-05-01 23:50:07 -0500

    Fixed Passenger fare values.

--- core/src/com/mygdx/game/Passenger.java
+++ core/src/com/mygdx/game/Passenger.java
@@ -48,22 +48,22 @@ public class Passenger {
                 break;
             case 4:
                 type = "Poor";
-                fare = setFare(7);
+                fare = setFare(15);
                 sprite = new Sprite(new Texture("images/poor_person.png"));
                 break;
             case 5:
                 type = "Poor";
-                fare = setFare(7);
+                fare = setFare(15);
                 sprite = new Sprite(new Texture("images/poor_person2.png"));
                 break;
             case 6:
                 type = "Richie";
-                fare = setFare(15);
+                fare = setFare(7);
                 sprite = new Sprite(new Texture("images/richie.png"));
                 break;
             case 7:
                 type = "Richie";
-                fare = setFare(15);
+                fare = setFare(7);
                 sprite = new Sprite(new Texture("images/richie2.png"));
                 break;
         }
